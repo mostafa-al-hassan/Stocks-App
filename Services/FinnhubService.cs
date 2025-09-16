b8aae7ee8ee406bcc05f55a0f41b159aa61d3ddf
mostafa-al-hassan <mostafadaud014014@gmail.com>
2025-09-16 15:48:22 +0300

    first commit Skeleton of the project along with Finnhub connection using configuration token (using user Secrets)

---
+++ Services/FinnhubService.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Tasks;
+using System.Text.Json;
+using StocksApp.ServiceContracts;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Http;
+
+namespace StocksApp.Services
+{
+    public class FinnhubService : IFinnhubService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IConfiguration _configuaration;
+
+        public FinnhubService(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuaration = configuration;
+        }
+
+
+        public async Task<Dictionary<string, object>?> GetStockPriceQuote(string stockSymbol)
+        {
+
+            using (HttpClient httpClient = _httpClientFactory.CreateClient())
+            {
+
+
+                HttpRequestMessage httpRequestMessage = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri($"https://finnhub.io/api/v1/quote?symbol={stockSymbol}&token={_configuaration["FinnhubToken"]}"),
+                    Method = HttpMethod.Get
+                };
+
+                HttpResponseMessage httpResponseMessage =
+                        await httpClient.SendAsync(httpRequestMessage);
+
+                Stream stream = httpResponseMessage.Content.ReadAsStream();
+
+                StreamReader streamReader = new StreamReader(stream);
+                string response = streamReader.ReadToEnd();
+
+                Dictionary<string, object>? responseDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(response);
+
+                if (responseDictionary == null)
+                {
+                    throw new InvalidOperationException();
+                }
+                else if (responseDictionary.ContainsKey("error"))
+                {
+                    throw new InvalidOperationException(Convert.ToString(responseDictionary["error"]));
+                }
+                return responseDictionary;
+            } // here closes the connection 
+        }
+    }
+}
